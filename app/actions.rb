395f5f6e69f2b9c25e9c242740e192363ce3a1fa
grrilla <mike.longauer@gmail.com>
2016-08-08 20:27:23 +0000

    Add new track form and post action

--- app/actions.rb
+++ app/actions.rb
@@ -3,7 +3,24 @@ get '/' do
   redirect '/tracks'
 end
 
+post '/tracks/new' do
+  @track = Track.new(
+    title: params[:title],
+    author: params[:author],
+    url: params[:url]
+  )
+  if @track.save
+    redirect '/tracks'
+  else
+    erb :'/tracks/new'
+  end
+end
+
 get '/tracks' do
   @tracks = Track.all
   erb :'/tracks/index'
+end
+
+get '/tracks/new' do
+  erb :'/tracks/new'
 end
